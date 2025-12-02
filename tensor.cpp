14dd3a47c317430e0ee767cded60265d59b9af22
keanulim <keanu7k@gmail.com>
2025-12-02 08:29:16 -0800

    constructors added

--- tensor.cpp
+++ tensor.cpp
@@ -2,6 +2,19 @@
 // Created by Keanu Lim on 11/29/25.
 //
 #include "tensor.h"
+
+template<typename T>
+Tensor<T>::Tensor(std::vector<int64_t> user_shape)
+    :data{0}, shape{user_shape}, strides{shape.size(),1} {
+    compute_strides();
+};
+
+template<typename T>
+Tensor<T>::Tensor(std::vector<int64_t> user_data, std::vector<int64_t> user_shape)
+    :data{user_data}, shape{user_shape}, strides{shape.size(),1}{
+    compute_strides();
+};
+
 template <typename T>
 int64_t Tensor<T>::size() const{
     int64_t total = 1;
@@ -14,12 +27,8 @@ int64_t Tensor<T>::size() const{
 
 template<typename T>
 void Tensor<T>::compute_strides() {
-    for (int i = shape.size()-1; i > 0; i--) {
-        if (i == shape.size()-1) {
-            strides[i] = 1;
-        }
-        else {
-            strides[i] = shape[i+1] * strides[i+1];
-        }
+    std::vector<int64_t> temp(shape.size(),1);
+    for (int i = shape.size()-1; i > 0; --i) {
+        strides[i] = shape[i+1] * strides[i+1];
     }
 }
--- tensor.h
+++ tensor.h
@@ -11,13 +11,13 @@ template<typename T>
 class Tensor {
 private:
     std::vector<T> data;
-    std::vector<int64_t> shape;
+    std::vector<T> shape;
     std::vector<int64_t> strides;
     void compute_strides();
 public:
     int64_t size() const;
-
-
+    explicit Tensor(std::vector<int64_t> user_shape); // constructor shape only
+    Tensor(std::vector<int64_t> user_data, std::vector<int64_t> user_shape); //shape and data
 };
 
 #endif //MINI_TENSOR_INFERENCE_ENGINE_TENSOR_H
