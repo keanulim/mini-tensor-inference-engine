41a5b7f76d6b11ae18ba68efd23dc706afca9856
keanulim <keanu7k@gmail.com>
2025-12-02 09:32:52 -0800

    completed first version of tensor class

--- tensor.cpp
+++ tensor.cpp
@@ -4,31 +4,45 @@
 #include "tensor.h"
 
 template<typename T>
-Tensor<T>::Tensor(std::vector<int64_t> user_shape)
-    :data{0}, shape{user_shape}, strides{shape.size(),1} {
+Tensor<T>::Tensor(const std::vector<int64_t>& user_shape)
+    :shape(user_shape), data(size()), strides(shape.size()) {
     compute_strides();
 };
 
 template<typename T>
-Tensor<T>::Tensor(std::vector<int64_t> user_data, std::vector<int64_t> user_shape)
-    :data{user_data}, shape{user_shape}, strides{shape.size(),1}{
+Tensor<T>::Tensor(const std::vector<int64_t>& user_shape, const std::vector<T>& user_data)
+    :shape(user_shape), data(user_data), strides(shape.size()){
+    assert(user_data.size()==size());
     compute_strides();
 };
 
 template <typename T>
 int64_t Tensor<T>::size() const{
     int64_t total = 1;
-    for (int i{0}; i < shape.size(); i++) {
+    for(int64_t i{0}; i < shape.size(); i++) {
         total *= shape[i];
     }
     return total;
-
 }
 
 template<typename T>
 void Tensor<T>::compute_strides() {
-    std::vector<int64_t> temp(shape.size(),1);
-    for (int i = shape.size()-1; i > 0; --i) {
+    if (shape.empty()) return;
+
+    strides[shape.size()-1] = 1;
+    for (int64_t i = shape.size()-2; i >= 0; --i) {
         strides[i] = shape[i+1] * strides[i+1];
     }
 }
+
+template<typename T>
+int64_t Tensor<T>::compute_index(const std::vector<int64_t>& index) const{
+    assert(index.size()==shape.size());
+    int64_t offset{0};
+    for (int64_t i{0}; i < index.size(); ++i) {
+        assert(index[i] >= 0 && index[i] < shape[i]);
+        offset+= strides[i]*index[i];
+    }
+    return offset;
+}
+
--- tensor.h
+++ tensor.h
@@ -10,14 +10,16 @@
 template<typename T>
 class Tensor {
 private:
+    std::vector<int64_t> shape;
     std::vector<T> data;
-    std::vector<T> shape;
     std::vector<int64_t> strides;
     void compute_strides();
 public:
+    Tensor() = default;
+    explicit Tensor(const std::vector<int64_t>& user_shape); // constructor shape only
+    Tensor(const std::vector<int64_t>& user_shape, const std::vector<T>& user_data); //shape and data
     int64_t size() const;
-    explicit Tensor(std::vector<int64_t> user_shape); // constructor shape only
-    Tensor(std::vector<int64_t> user_data, std::vector<int64_t> user_shape); //shape and data
+    int64_t compute_index(const std::vector<int64_t>& index) const;
 };
 
 #endif //MINI_TENSOR_INFERENCE_ENGINE_TENSOR_H
