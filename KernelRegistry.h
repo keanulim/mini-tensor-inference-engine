0ca4cbf64368932819a6d2bd8ad5dec73306595a
keanulim <keanu7k@gmail.com>
2025-12-05 23:57:46 -0800

    KernelRegistry header file

---
+++ KernelRegistry.h
@@ -0,0 +1,20 @@
+//
+// Created by Keanu Lim on 12/5/25.
+//
+
+#ifndef MINI_TENSOR_INFERENCE_ENGINE_KERNELREGISTRY_H
+#define MINI_TENSOR_INFERENCE_ENGINE_KERNELREGISTRY_H
+
+#include <map>
+#include <string>
+
+
+class KernelRegistry {
+private:
+    std::map<std::string, KernelFn> registry_;
+public:
+    static void register_kernel(const std::string& op_type, KernelFn kernel_func);
+    static KernelFn lookup_kernel(const std::string& op_type);
+};
+
+#endif //MINI_TENSOR_INFERENCE_ENGINE_KERNELREGISTRY_H
